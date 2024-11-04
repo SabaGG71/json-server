3fa97e472b36bc15dcc4ab40e9a6c6c149fadb33
Saba <sgachechiladze881@gmail.com>
2024-11-05 01:06:15 +0400

    Add gitignoreeeeeeeee

--- api/index.js
+++ api/index.js
@@ -1,10 +1,13 @@
-const path = require("path");
+// api/index.js
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router(path.join(__dirname, "..", "db.json")); // Modify path here
+const dbPath = process.env.DB_PATH || "db.json"; // Fallback to 'db.json'
+const router = jsonServer.router(path.join(__dirname, "..", dbPath));
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 server.use(router);
 
-module.exports = (req, res) => server(req, res);
+server.listen(3000, () => {
+  console.log("JSON Server is running");
+});
